11e79744c27ec7b567ed4fe11949ac45fb018291
Vijay Chaurasiya <98342941+vijayc9@users.noreply.github.com>
2025-01-16 14:15:51 +0530

    Revert "Dashboard Chat Assistant"

--- agentneo/ui/src/App.tsx
+++ agentneo/ui/src/App.tsx
@@ -1,13 +1,3 @@
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { SidebarProvider } from './contexts/SidebarContext';
-import { ProjectProvider } from './contexts/ProjectContext';
-import Overview from './pages/Overview';
-import Analysis from './pages/Analysis';
-import TraceHistory from './pages/TraceHistory';
-import Evaluation from './pages/Evaluation';
-import ChatWidget from './components/ChatWidget';
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { SidebarProvider } from './contexts/SidebarContext'
@@ -20,20 +10,6 @@ import { ThemeProvider } from '@/theme/ThemeProvider'
 
 function App () {
   return (
-    <ProjectProvider>
-      <SidebarProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Overview />} />
-            <Route path="/analysis" element={<Analysis />} />
-            <Route path="/trace-history" element={<TraceHistory />} />
-            <Route path="/evaluation" element={<Evaluation />} />
-          </Routes>
-        </Router>
-        <ChatWidget/>
-      </SidebarProvider>
-    </ProjectProvider>
-  );
     <ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
       <ProjectProvider>
         <SidebarProvider>
