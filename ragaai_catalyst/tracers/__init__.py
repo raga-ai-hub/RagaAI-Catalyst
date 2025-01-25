24aa065e78c72a3d38d2c8c51324b8d93d3b1302
kiranscaria <kiranscaria@outlook.com>
2025-01-25 12:07:09 +0530

    add code for distributed usage of tracers
    Signed-off-by: kiranscaria <kiranscaria@outlook.com>

--- ragaai_catalyst/tracers/__init__.py
+++ ragaai_catalyst/tracers/__init__.py
@@ -1,3 +1,17 @@
 from .tracer import Tracer
+from .distributed import (
+    init_tracing,
+    trace_agent,
+    trace_llm,
+    trace_tool,
+    current_span,
+)
 
-__all__ = ["Tracer"] 
+__all__ = [
+    "Tracer",
+    "init_tracing",
+    "trace_agent", 
+    "trace_llm",
+    "trace_tool",
+    "current_span"
+]
