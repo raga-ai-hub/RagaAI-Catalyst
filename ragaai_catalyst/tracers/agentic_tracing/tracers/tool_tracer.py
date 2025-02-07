a91573d8084f9db4fe7ad222a7cf66cf01253394
Tushar Kumar <tushar.kumar@raga.ai>
2025-02-07 07:06:06 +0530

    bug fix

--- ragaai_catalyst/tracers/agentic_tracing/tracers/tool_tracer.py
+++ ragaai_catalyst/tracers/agentic_tracing/tracers/tool_tracer.py
@@ -384,7 +384,7 @@ class ToolTracerMixin:
         self.start_component(component_id)
         try:
             # Execute the tool
-            result = await self.file_tracker.trace_wrapper(func(*args, **kwargs))
+            result = await self.file_tracker.trace_wrapper(func)(*args, **kwargs)
 
             # Calculate resource usage
             end_memory = psutil.Process().memory_info().rss
