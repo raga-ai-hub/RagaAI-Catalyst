23302afea2f8687004edb1304c3e93c8c00366e9
Jyotsana <jyotsana@raga.ai>
2025-02-26 12:46:54 +0530

    Added support for litellm cost changes

--- ragaai_catalyst/tracers/agentic_tracing/upload/trace_uploader.py
+++ ragaai_catalyst/tracers/agentic_tracing/upload/trace_uploader.py
@@ -525,7 +525,7 @@ def ensure_uploader_running():
                 pid = os.fork()
                 if pid > 0:
                     # Exit from second parent
-                    sys.exit(0)
+                    os._exit(0)
                     
                 # Redirect standard file descriptors
                 sys.stdout.flush()
