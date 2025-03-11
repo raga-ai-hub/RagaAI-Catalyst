78c15aa7d739b7df4710a95cc79d0fe622fc7a94
vijayc9 <vijayc2306@gmail.com>
2025-03-11 15:48:35 +0530

    modify examples

--- examples/smolagents_examples/rag_using_chromadb/rag_using_chromadb.py
+++ examples/smolagents_examples/rag_using_chromadb/rag_using_chromadb.py
@@ -1,23 +1,41 @@
+import sys
+sys.path.append('.')
+
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 import datasets
 from langchain.docstore.document import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
-
 from langchain_huggingface import HuggingFaceEmbeddings
+from smolagents import LiteLLMModel, Tool
+from smolagents.agents import CodeAgent
 from tqdm import tqdm
 from transformers import AutoTokenizer
 
-from smolagents import LiteLLMModel, Tool
-from smolagents.agents import CodeAgent
+from ragaai_catalyst import RagaAICatalyst, Tracer, init_tracing
 
+catalyst = RagaAICatalyst(
+    access_key='1q2igAYCIlpSBufkdB6f',
+    secret_key='yG6TJOgES8D9jAi9OI0X6SgvZNtkcFvkOruukJay',
+    base_url="https://llm-dev5.ragaai.ai/api",
+)
 
+tracer = Tracer(
+    project_name="integration_exp",
+    dataset_name="smolagents_rag_using_chromadb",
+    tracer_type="agentic/smolagents",
+)
+
+init_tracing(catalyst=catalyst, tracer=tracer)
 
 knowledge_base = datasets.load_dataset("m-ric/huggingface_doc", split="train")
 
 source_docs = [
-    Document(page_content=doc["text"], metadata={"source": doc["source"].split("/")[1]}) for doc in knowledge_base
+    Document(page_content=doc["text"], metadata={"source": doc["source"].split("/")[1]})
+    for doc in knowledge_base
 ]
 
 text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
@@ -40,18 +58,20 @@ for doc in tqdm(source_docs):
             docs_processed.append(new_doc)
 
 
-print("Embedding documents... This should take a few minutes (5 minutes on MacBook with M1 Pro)")
+print(
+    "Embedding documents... This should take a few minutes (5 minutes on MacBook with M1 Pro)"
+)
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 
-vector_store = Chroma.from_documents(docs_processed, embeddings, persist_directory="./chroma_db")
+vector_store = Chroma.from_documents(
+    docs_processed, embeddings, persist_directory="./chroma_db"
+)
 
 
 class RetrieverTool(Tool):
     name = "retriever"
-    description = (
-        "Uses semantic search to retrieve the parts of documentation that could be most relevant to answer your query."
-    )
+    description = "Uses semantic search to retrieve the parts of documentation that could be most relevant to answer your query."
     inputs = {
         "query": {
             "type": "string",
@@ -68,7 +88,10 @@ class RetrieverTool(Tool):
         assert isinstance(query, str), "Your search query must be a string"
         docs = self.vector_store.similarity_search(query, k=3)
         return "\nRetrieved documents:\n" + "".join(
-            [f"\n\n===== Document {str(i)} =====\n" + doc.page_content for i, doc in enumerate(docs)]
+            [
+                f"\n\n===== Document {str(i)} =====\n" + doc.page_content
+                for i, doc in enumerate(docs)
+            ]
         )
 
 
@@ -85,10 +108,9 @@ agent = CodeAgent(
     verbosity_level=2,
 )
 
+if __name__ == "__main__":
+    query = "How can I push a model to the Hub?"
+    agent_output = agent.run(query)
 
-
-query = "How can I push a model to the Hub?"
-agent_output = agent.run(query)
-
-print("Final output:")
-print(agent_output)
+    print("Final output:")
+    print(agent_output)
